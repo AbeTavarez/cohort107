bbae4d3d1805f799cc12550081dd89d9f9ffe516
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-11-14 17:53:09 -0500

    js conditionals

--- javascript/variables/script.js
+++ javascript/variables/script.js
@@ -26,3 +26,41 @@ console.log(typeof msg);
 console.log(typeof favoriteNum);
 
 console.log(typeof 'Javascript');
+
+console.log('========== String Literals ===========');
+
+const frost =
+	"Nature's first green is gold, \
+Her hardest hue to hold. \
+Her early leaf's a flower; \
+But only so an hour. \
+Then leaf subsides to leaf. \
+So Eden sank to grief, \
+So dawn goes down to day. \
+Nothing gold can stay.";
+
+console.log(frost);
+
+
+const quote = 'Nature\'s first green is gold, \nHer hardest hue to hold.';
+console.log(quote);
+
+const frost2 =
+	`Nature's first green is gold,
+Her hardest hue to hold.
+Her early leaf's a flower;
+But only so an hour.
+Then leaf subsides to leaf.
+So Eden sank to grief,
+So dawn goes down to day.
+Nothing gold can stay.`;
+
+console.log(frost2);
+
+
+const bill = 100;
+const tax = 30;
+
+console.log("The bill is " + bill + ", plus tax " + tax + " for a total of " + "$" + (bill + tax));
+
+console.log(`The bill is ${bill}, 'plus tax' ${tax} for a "total" of $${bill + tax}.`);
