dd5ad41618f87a0ec87275ff4af1375bc2f27a0e
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-11-16 16:03:47 -0500

    loops

---
+++ javascript/loops/index.js
@@ -0,0 +1,50 @@
+console.log('Running');
+
+// for (let counter = 0; counter < 10; counter++) {
+//     console.log(`Counter is: ${counter}`);
+// }
+
+console.log('Some other code');
+
+// count down from 10 to 1
+// for (let i = 10; i >= 1; i--) {
+//     console.log(`I is : ${i}`);
+// }
+
+// Output even number from 1 to 10.
+// for (let i = 1; i <= 10; i++) {
+
+//     console.log(`Now checking ${i}`);
+
+//     if (i % 2 == 0) {
+//         console.log(`${i} is even!`);
+//     } else {
+//         console.log(`${i} is odd!`);
+
+//     }
+// }
+
+// Output multiples of 3, starting at 6 and ending at 60.
+// for (let i = 6; i <= 60; i++) {
+//     if (i % 3 === 0) {
+//         console.log(`${i} is a multiple of 3`);
+//     } else {
+//         console.log(`${i} is NOT a multiple of 3`);
+//     }
+// }
+
+//Output an increasing number of # symbols, from 1 to 7, as shown below.
+// let symbol = '';
+
+// for (let i = 1; i <= 7; i++) {
+
+//     symbol+= '#';
+
+//     console.log(symbol);
+// }
+
+let str = "Hello World";
+
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+}
