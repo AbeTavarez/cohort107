825d6bad1ee431954329b493fcdbd13c109b5484
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-01-25 16:03:45 -0500

    learner app

---
+++ React/learner-app/src/App.js
@@ -0,0 +1,17 @@
+import {learnerData} from './data/learnerData';
+// import useState
+
+import './App.css';
+
+function App() {
+  // create a new state passing the learnerData
+  
+  // const [learners, setLearners] = useState(learnerData)
+  return (
+    <div className="App">
+      <h1>Learner App</h1>
+    </div>
+  );
+}
+
+export default App;
