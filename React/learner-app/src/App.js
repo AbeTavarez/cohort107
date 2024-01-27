1ba0a9619bfb108f957cc3acdf168aa532285889
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-01-26 19:54:31 -0500

    data imported

--- React/learner-app/src/App.js
+++ React/learner-app/src/App.js
@@ -1,15 +1,17 @@
 import {learnerData} from './data/learnerData';
-// import useState
+import {useState} from 'react';
 
 import './App.css';
 
 function App() {
-  // create a new state passing the learnerData
-  
-  // const [learners, setLearners] = useState(learnerData)
+  // State
+  const [learners, setLearners] = useState(learnerData);
+
   return (
     <div className="App">
       <h1>Learner App</h1>
+
+     
     </div>
   );
 }
