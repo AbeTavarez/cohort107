4cfe378732f34b103c840b88a06c2641e7107d96
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-02-09 00:04:08 -0500

    var

---
+++ tk/vars/script.js
@@ -0,0 +1,18 @@
+// === JavaScript Variables === //
+
+// var
+var error = "I'm too old for this.";
+
+// let
+let accountBalance = 1000;
+let status = "online";
+let username = "dev4life";
+let gameOver = true;
+
+
+// const
+const dob = "2-9-2023";
+const userId = "xyz459";
+const maxHealth = 500;
+const userInfo = { name: "John" };
+const friends = ["Mike", "Hanna"];
