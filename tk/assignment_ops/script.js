01a933301f9e85fd9127b942afc8d796e2b6770a
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-02-09 22:08:26 -0500

    assignment ops

---
+++ tk/assignment_ops/script.js
@@ -0,0 +1,36 @@
+// JS Assignment Operators
+
+// Assignment
+let myValue = 55
+console.log(myValue); // outputs 55
+
+// Addition + Assignment
+let a = 2
+a = a + 3
+// a += 3
+console.log(a); // outputs 5
+
+// Subtracting + Assignment
+let b = 5 
+b -= 2
+console.log(b); // outputs 3
+
+// Multiplication + Assignment
+let c = 5 
+c *= 2
+console.log(c); // outputs 10
+
+// Division + Assignment
+let i = 5 
+i /= 2
+console.log(i); // outputs 2.5
+
+// Modulus + Assignment
+let j = 5 
+j %= 2
+console.log(j); // outputs 1
+
+// Exponentiation + Assignment
+let x = 5 
+x **= 2
+console.log(x); // outputs 25
