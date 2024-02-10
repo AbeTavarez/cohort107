2deb61be1b0b68b202b4db059a1b432ccc8583d9
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-02-09 22:09:07 -0500

    ops

---
+++ tk/operations/script.js
@@ -0,0 +1,35 @@
+// JS Math Operators
+
+// Addition
+let a = 2 + 3
+console.log(a); // outputs 5
+
+// Subtracting
+let b = 5 - 2
+console.log(b); // outputs 3
+
+// Multiplication
+let c = 5 * 2
+console.log(c); // outputs 10
+
+// Division
+let i = 5 / 2
+console.log(i); // outputs 2.5
+
+// Modulus or Remainder
+let j = 5 % 2
+console.log(j); // outputs 1
+
+// Exponentiation
+let x = 5 ** 2
+console.log(x); // outputs 25
+
+// Increment
+let y = 3
+y++
+console.log(y); // outputs 4
+
+// Increment
+let z = 4
+z--
+console.log(z); // outputs 3
