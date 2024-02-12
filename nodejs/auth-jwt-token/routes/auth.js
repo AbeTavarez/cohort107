da14255505b829ef25bc46e4e6be169daea0e9d9
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-02-11 23:28:27 -0500

    verify token, protedted routes

--- nodejs/auth-jwt-token/routes/auth.js
+++ nodejs/auth-jwt-token/routes/auth.js
@@ -1,40 +1,66 @@
-import {Router} from 'express';
-import jwt from 'jsonwebtoken';
-import User from '../models/user.js';
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+import verifyToken from "../middlewares/verifyToken.js";
 
 const router = new Router();
 
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // checks if user already exist in db
+    const userExist = await User.findOne({ email });
+    console.log(userExist);
 
-router.post('/signup', async (req, res) => {
-    const {email, password} = req.body;
-    try {
-       const userExist = await User.findOne({email});
-       console.log(userExist)
-
-       if (userExist) {
-        return res.status(400).json({msg: "User already exist!"})
-       }
-
-       const user = await User.create({email, password});
-       console.log(user);
-
-       const token = jwt.sign({payload: user}, process.env.SECRET ,{expiresIn: '24h'});
-       
-       res.status(201).json(token);
-    } catch (error) {
-        console.log(error);
+    // if true return
+    if (userExist) {
+      return res.status(400).json({ msg: "User already exist!" });
     }
-    
+
+    // create new user
+    const user = await User.create({ email, password });
+    console.log(user);
+
+    // create a new token and add user as payload
+    const token = jwt.sign({ user }, process.env.SECRET, {
+      expiresIn: "24h",
+    });
+
+    res.status(201).json(token);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
 
-router.post('/signin', async(req, res) => {
-    try {
-        res.send('sign-in');
-    } catch (error) {
-        console.log(error);
+  try {
+    // find user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: "Check Credentials!" });
     }
+
+    // check password
+    // if using bcrypt, use the bcrypt.compare method
+    const passwordMatched = password === user.password;
+    if (!passwordMatched) {
+      return res.status(400).json({ msg: "Check credentials!" });
+    }
+
+    // create a new token and add user as payload
+    const token = jwt.sign({ user }, process.env.SECRET, { expiresIn: "24h" });
+
+    res.status(201).json(token);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
+router.post("/change-password", verifyToken, async (req, res) => {
+  console.log(req.user);
+  res.send("Token verified!");
+});
 
 export default router;
